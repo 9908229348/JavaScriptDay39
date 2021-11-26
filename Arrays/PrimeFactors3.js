f27427fb7f5835ae978635f941c3f1db634062e3
9908229348 <kalavaisravanthi98@gmail.com>
2021-11-26 12:02:47 +0530

    Added: Problems based on Arrays

---
+++ Arrays/PrimeFactors3.js
@@ -0,0 +1,26 @@
+function getPrimeFactors(number){
+    let isPrime = 0;
+    let i,j;
+    let count = 0, array = [];
+    for(i = 2; i <= number; i++)
+    {
+        if(number % i == 0)			
+        {
+            isPrime=1;
+            for(j = 2; j <= (i / 2); j++)		
+            {
+                if((i % j) == 0)		
+                    isPrime = 0;
+            }
+            if(isPrime == 1)
+            {
+                array[count++] = i;
+            }
+        }
+    }
+    return array;
+}
+
+const prompt = require("prompt-sync")();
+const number = prompt('Enter number: ');
+console.log("Prime factors of " + number + " are " + getPrimeFactors(number));
